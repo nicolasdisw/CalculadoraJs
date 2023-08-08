bbd01dc55f140782c152bf79f7db62725677f7ea
proyecto <nicogabriellacroix@gmail.com>
2023-08-08 00:45:08 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Esto agrega a la pantalla el valor del boton de la calculadora que toquemos
+
+function agregar(valor){
+    document.getElementById('pantalla').value += valor
+}
+// Esta accion vacia la pantalla
+
+function borrar(){
+    document.getElementById('pantalla').value = ""
+}
+
+// Esta accion toma el valor de la pantalla y ejecuta los calculos
+
+function calcular(){
+    const valorPantalla =document.getElementById('pantalla').value
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
+}
